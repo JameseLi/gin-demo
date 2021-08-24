f63a46053b803e29d25979f2471f605705112edf
李正钧 <lzj@umshz.com>
2021-08-24 11:45:42 +0800

    config demo

---
+++ config-gin/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"log"
+)
+
+type Server struct {
+	Env string `yaml:"env"`
+}
+
+func NewServer() *Server {
+	return &Server{}
+}
+
+var filePath string
+
+func init() {
+	flag.StringVar(&filePath, "conf", "/Users/lizhengjun/gin-demo/config-gin/conf/test.yaml", "config 文件路径地址")
+}
+
+func main() {
+	flag.Parse()
+	fmt.Printf("filePath: %s\n", filePath)
+
+	yamlFile, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//初始化配置文件
+	conf := NewServer()
+	err = yaml.Unmarshal(yamlFile, conf)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r := gin.Default()
+
+	r.GET("/config", func(context *gin.Context) {
+		context.JSON(200, gin.H{
+			"env": conf.Env,
+		})
+	})
+
+	err = r.Run(":8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
